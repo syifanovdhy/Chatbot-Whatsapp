8f076f45403619b3934a40d5b8916eaa1ce53bc9
syifanovdhy <syifanovdhy@gmail.com>
2026-06-04 11:14:58 +0800

    fake db consultation

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ app = FastAPI()
 fake_users_db = []
 next_user_id =1
 
+fake_consultation_db = []
+next_consultation_id = 1
+
+class Consultation(BaseModel):
+    user_id: int
+    keperluan: str
 
 class User(BaseModel):
     nama: str
@@ -47,3 +53,24 @@ def get_user(user_id: int):
         if user["id"] == user_id:
             return user
     return {"message": "User not found"}
+
+@app.post("/consultations")
+def create_consultation(consultation: Consultation):
+
+    global next_consultation_id
+
+    new_consultation = {
+        "id": next_consultation_id,
+        "user_id": consultation.user_id,
+        "keperluan": consultation.keperluan,
+        "status": "menunggu"
+    }
+
+    fake_consultation_db.append(new_consultation)
+    next_consultation_id += 1
+
+    return new_consultation
+
+@app.get("/consultations")
+def get_consultations():
+    return fake_consultation_db
