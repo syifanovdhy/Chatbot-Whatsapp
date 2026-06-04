a8d2592b3925ef683be22cf73563a82253112320
syifanovdhy <syifanovdhy@gmail.com>
2026-06-04 11:02:56 +0800

    fake database python

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
+
+fake_users_db = []
+next_user_id =1
+
+
 class User(BaseModel):
     nama: str
-    instansi: str
     email: str
-    nomor_wa: str
 
 @app.get("/")
 def home():
@@ -17,14 +20,30 @@ def home():
 
 @app.post("/users")
 def create_user(user: User):
-    return {
+
+    global next_user_id
+
+    new_user = {
+        "id": next_user_id,
         "nama": user.nama,
         "email": user.email
     }
 
+
+    fake_users_db.append(new_user)
+    next_user_id += 1
+
+    return {
+        "message": "User berhasil ditambahkan"
+    }
+
+@app.get("/users")
+def get_users():
+    return fake_users_db
+
 @app.get("/users/{user_id}")
 def get_user(user_id: int):
-    return {
-        "user_id": user_id,
-        "nama": "Altair"
-    }
+    for user in fake_users_db:
+        if user["id"] == user_id:
+            return user
+    return {"message": "User not found"}
