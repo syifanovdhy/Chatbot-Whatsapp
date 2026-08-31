64402d907035968fd0af132fa7db99f6ef3d8e43
syifanovdhy <syifanovdhy@gmail.com>
2026-08-31 19:18:07 +0800

    backup database

--- services/publication_service.py
+++ services/publication_service.py
@@ -139,24 +139,23 @@ def get_publication_menu():
 
 Ketikkan Kode Publikasi yang ingin anda akses.
 
-DDA. Kabupaten Banggai Kepulauan Dalam Angka
-PDRBL. Produk Domestik Regional Bruto Kabupaten Banggai Kepulauan Menurut Lapangan Usaha
-PDRBP. Produk Domestik Regional Bruto Kabupaten Banggai Kepulauan Menurut Pengeluaran
-Statkesra. Statistik Kesejahteraan Rakyat
-SKD. Analisis Hasil Survei Kebutuhan Data BPS Kabupaten Banggai Kepulauan
-
-KCA30. Kecamatan Totikum Dalam Angka
-KCA31. Kecamatan Totikum Selatan Dalam Angka
-KCA40. Kecamatan Tinangkung Dalam Angka
-KCA41. Kecamatan Tinangkung Selatan Dalam Angka
-KCA42. Kecamatan Tinangkung Utara Dalam Angka
-KCA50. Kecamatan Liang Dalam Angka
-KCA51. Kecamatan Peling Tengah Dalam Angka
-KCA60. Kecamatan Bulagi Dalam Angka
-KCA61. Kecamatan Bulagi Selatan Dalam Angka
-KCA62. Kecamatan Bulagi Utara Dalam Angka
-KCA70. Kecamatan Buko Dalam Angka
-KCA71. Kecamatan Buko Selatan Dalam Angka
+- DDA. Kabupaten Banggai Kepulauan Dalam Angka
+- PDRBL. Produk Domestik Regional Bruto Kabupaten Banggai Kepulauan Menurut Lapangan Usaha
+- PDRBP. Produk Domestik Regional Bruto Kabupaten Banggai Kepulauan Menurut Pengeluaran
+- Statkesra. Statistik Kesejahteraan Rakyat
+- SKD. Analisis Hasil Survei Kebutuhan Data BPS Kabupaten Banggai Kepulauan
+- KCA30. Kecamatan Totikum Dalam Angka
+- KCA31. Kecamatan Totikum Selatan Dalam Angka
+- KCA40. Kecamatan Tinangkung Dalam Angka
+- KCA41. Kecamatan Tinangkung Selatan Dalam Angka
+- KCA42. Kecamatan Tinangkung Utara Dalam Angka
+- KCA50. Kecamatan Liang Dalam Angka
+- KCA51. Kecamatan Peling Tengah Dalam Angka
+- KCA60. Kecamatan Bulagi Dalam Angka
+- KCA61. Kecamatan Bulagi Selatan Dalam Angka
+- KCA62. Kecamatan Bulagi Utara Dalam Angka
+- KCA70. Kecamatan Buko Dalam Angka
+- KCA71. Kecamatan Buko Selatan Dalam Angka
 
 Ketik *Pub* untuk menampilkan kembali daftar Publikasi.
 Ketik *Menu* untuk kembali ke menu awal.
